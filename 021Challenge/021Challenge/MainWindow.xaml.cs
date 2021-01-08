b914af44b093a10a9e641c856e1d37d5cde0343c
Blinky1237 <70332455+Blinky1237@users.noreply.github.com>
2021-01-08 12:48:51 -0600

    i got the salary working

--- 021Challenge/021Challenge/MainWindow.xaml.cs
+++ 021Challenge/021Challenge/MainWindow.xaml.cs
@@ -26,35 +26,56 @@ namespace _21_challenge
             InitializeComponent();
         }
 
+       
+        public DateTime myFirstDate, mySecondDate;
+
+
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //helps format the phone and Social security numbers and salary
             double ss = double.Parse(textBoxSocialSecurity.Text);
             double phn = double.Parse(textBoxPhone.Text);
+            //double totalSalary = double.Parse((string)labelTotalPaydue.Content);
 
+            //Confi
+            int DaysOut = Convert.ToInt32((calendarEnd.SelectedDate.Value - calendarStart.SelectedDate.Value).Days);
+            
+           
+
+
+            //allows the user to input the requested items and will format as needed
             string result1 = string.Format("Driver - {0}", textBoxName.Text);
             string result2 = string.Format("Social Security #: {0:000-00-0000}", ss);
             string result3 = string.Format("Phone Number: {0:(000)000-0000}", phn);
             string result4 = string.Format("Total Miles:");
-
-            //double a = double.Parse(textBoxStartMiles.Text);
-            //double b = double.Parse(textBoxEndMiles.Text);
-            //double.
-
-
+            string result5 = string.Format("Total # of Days out: {0} ", DaysOut);
             
+         
+
+            //adds the miles and salary together as needed
             if (!string.IsNullOrEmpty(textBoxStartMiles.Text) && !string.IsNullOrEmpty(textBoxEndMiles.Text))
-            lableMilesResultNmbr.Content= (Convert.ToInt32(textBoxStartMiles.Text) + Convert.ToInt32(textBoxEndMiles.Text)).ToString();
+            { 
+            lableMilesResultNmbr.Content = (Convert.ToInt32(textBoxEndMiles.Text) - Convert.ToInt32(textBoxStartMiles.Text)).ToString();
+            }
+            if (!string.IsNullOrEmpty((string)lableMilesResultNmbr.Content))
+            {
+            }
             
-            
-            
+
+                
+            double salary = Convert.ToDouble((lableMilesResultNmbr.Content)) * .25;
+            string result6 = string.Format("Total pay Due - {0:c}", salary);
+
+
 
             //Labels the driver ticket
-            labelDriverResult_Copy.Content = result1;
+            labelDriverResult.Content = result1;
             labelSocialSecurityResult.Content = result2;
             labelPhoneResult.Content = result3;
             labelMilesResult.Content = result4;
-       
-
+            labelTotalDaysout.Content = result5;
+            labelTotalPaydue.Content = result6;
         }
 
     }
